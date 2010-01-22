3e02a4e7b48d805d3e97f4fda7b1b7af039d4e86
Bas van Dijk <v.dijk.bas@gmail.com>
2010-01-22 20:45:29 +0100

    Added support for bytestring IO
    Ignore-this: 8e3d8437619a3edbd12eb9cdf8460e67
    
    darcs-hash:20100122194529-ae560-635a11cce8739c384fb7ae66dd7d936d32747181.gz

---
+++ Data/ByteString/Lazy/Char8/SaferFileHandles.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE UnicodeSyntax, NoImplicitPrelude #-}
+
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  Data.ByteString.SaferFileHandles
+-- Copyright   :  (c) 2009 Bas van Dijk
+-- License     :  BSD3 (see the file LICENSE)
+-- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
+--
+-- This module lifts the bytestring IO operations into the region monad.
+--
+-------------------------------------------------------------------------------
+
+module Data.ByteString.Lazy.Char8.SaferFileHandles
+    ( hGetContents
+    , hGet
+    , hGetNonBlocking
+
+    , hPut
+    ) where
+
+-------------------------------------------------------------------------------
+-- Imports
+-------------------------------------------------------------------------------
+
+-- from base:
+import Data.Int ( Int )
+
+-- from transformers:
+import Control.Monad.Trans ( MonadIO )
+
+-- from bytestring:
+import qualified Data.ByteString.Lazy.Char8 as B
+
+-- from regions:
+import Control.Monad.Trans.Region ( ParentOf )
+
+-- from explicit-iomodes:
+import qualified Data.ByteString.Lazy.Char8.ExplicitIOModes as E
+    ( hGetContents, hGet, hGetNonBlocking
+    , hPut
+    )
+
+-- from ourselves:
+import System.IO.SaferFileHandles ( RegionalFileHandle , ReadModes, WriteModes )
+import System.IO.SaferFileHandles.Internal ( wrap, wrap2 )
+
+
+-------------------------------------------------------------------------------
+-- ByteString I/O with regional file handles
+-------------------------------------------------------------------------------
+
+-- | Wraps @Data.ByteString.@'B.hGetContents'.
+hGetContents ∷ (pr `ParentOf` cr, MonadIO cr, ReadModes ioMode)
+             ⇒ RegionalFileHandle ioMode pr → cr B.ByteString
+hGetContents = wrap E.hGetContents
+
+-- | Wraps @Data.ByteString.@'B.hGet'.
+hGet ∷ (pr `ParentOf` cr, MonadIO cr, ReadModes ioMode)
+     ⇒ RegionalFileHandle ioMode pr → Int → cr B.ByteString
+hGet = wrap2 E.hGet
+
+-- | Wraps @Data.ByteString.@'B.hGetNonBlocking'.
+hGetNonBlocking ∷ (pr `ParentOf` cr, MonadIO cr, ReadModes ioMode)
+                ⇒ RegionalFileHandle ioMode pr → Int → cr B.ByteString
+hGetNonBlocking = wrap2 E.hGetNonBlocking
+
+
+-- | Wraps @Data.ByteString.@'B.hPut'.
+hPut ∷ (pr `ParentOf` cr, MonadIO cr, WriteModes ioMode)
+     ⇒ RegionalFileHandle ioMode pr → B.ByteString → cr ()
+hPut = wrap2 E.hPut
+
+
+-- The End ---------------------------------------------------------------------
--- System/IO/SaferFileHandles.hs
+++ System/IO/SaferFileHandles.hs
@@ -255,15 +255,13 @@ module System.IO.SaferFileHandles
 
 -- from base:
 import Prelude       ( Integer )
-import Control.Monad ( fmap
-                     , return, (>>=), fail
+import Control.Monad ( return, (>>=), fail
                      )
 import Data.Function ( ($) )
-import Data.Tuple    ( uncurry )
 import Data.Bool     ( Bool(False, True) )
 import Data.Char     ( Char, String )
 import Data.Int      ( Int )
-import Data.Maybe    ( Maybe(Nothing, Just) )
+import Data.Maybe    ( Maybe(Just) )
 import Text.Show     ( Show )
 import Text.Read     ( Read )
 import Foreign.Ptr   ( Ptr )
@@ -281,12 +279,7 @@ import Control.Monad.Trans ( MonadIO, liftIO )
 
 -- from regions:
 import Control.Monad.Trans.Region -- (re-exported entirely)
-import Control.Monad.Trans.Region.Unsafe ( Resource
-                                         , openResource
-                                         , closeResource
-                                         , internalHandle
-                                         )
-import qualified Control.Monad.Trans.Region.Unsafe as R ( Handle )
+import Control.Monad.Trans.Region.Unsafe ( internalHandle )
 
 -- from explicit-iomodes
 import System.IO.ExplicitIOModes ( IOMode(..)
@@ -295,7 +288,6 @@ import System.IO.ExplicitIOModes ( IOMode(..)
                                  , WriteModes
                                  -- TODO:, CheckMode
 
-                                 , IO
                                  , FilePath
                                  , BufferMode(..)
                                  , HandlePosn
@@ -319,96 +311,22 @@ import System.IO.ExplicitIOModes ( IOMode(..)
 
 import qualified System.IO.ExplicitIOModes as E
 
+-- from ourselves:
+import System.IO.SaferFileHandles.Internal
+
+-- TODO: explicit import:
+-- import System.IO.SaferFileHandles.Internal ( RegionalFileHandle
+--                                            , File(..)
+--                                            , Binary
+--                                            , Template
+--                                            , Standard(..)
+--                                            , wrap, wrap2, wrap3
+--                                            )
+
+
 -------------------------------------------------------------------------------
 -- * Files with explicit IO modes as scarce resources
 -------------------------------------------------------------------------------
-
-{-| A file scarce resource parameterized by the IOMode in which you want to open
-the file.
-
-Note that this module provides an instance for 'Resource' for 'File' @ioMode@. This allows
-you to 'open' files in a region which are automatically closed when the region
-terminates but it disallows you to return handles to these closed files from the
-region so preventing I/O with closed files.
--}
-data File ioMode where
-    File ∷ Binary → FilePath → IOMode ioMode → File ioMode
-    TempFile ∷ Binary
-             → FilePath
-             → Template
-#if MIN_VERSION_base(4,2,0)
-             → DefaultPermissions
-#endif
-             → File RW
-
-          -- TODO: I need to review the handling of standard files:
-    Std ∷ Standard ioMode → File ioMode
-
--- | Should the file be opened in binary mode?
-type Binary = Bool
-
--- | The template of a temporary file path.
-type Template = String
-
-#if MIN_VERSION_base(4,2,0)
--- | Should default permissions be used when opening a temporary file?
-type DefaultPermissions = Bool
-#endif
-
--- | The standard files parameterized by concrete IOModes which work for the
--- majority of cases.
-data Standard ioMode where
-    In  ∷ Standard R
-    Out ∷ Standard W
-    Err ∷ Standard W
-
--- | Internally used function to convert a standard file to the corresponding
--- handle.
-stdHndl ∷ Standard ioMode → E.Handle ioMode
-stdHndl In  = E.stdin
-stdHndl Out = E.stdout
-stdHndl Err = E.stderr
-
-instance Resource (File ioMode) where
-    data R.Handle (File ioMode) = FileHandle (Maybe FilePath)
-                                    -- The optional file path is needed
-                                    -- because opening a temporary file
-                                    -- also yields the generated file
-                                    -- path.
-                                    (E.Handle ioMode)
-
-    openResource (File isBinary filePath ioMode) =
-        fmap (FileHandle Nothing) $
-             (if isBinary then E.openBinaryFile else E.openFile)
-             filePath ioMode
-
-#if MIN_VERSION_base(4,2,0)
-    openResource (TempFile isBinary filePath template defaultPerms) = do
-        fmap (uncurry (FileHandle ∘ Just)) $
-             (case (isBinary, defaultPerms) of
-               (False, False) → E.openTempFile
-               (True,  False) → E.openBinaryTempFile
-               (False, True)  → E.openTempFileWithDefaultPermissions
-               (True,  True)  → E.openBinaryTempFileWithDefaultPermissions
-             ) filePath template
-#else
-    openResource (TempFile isBinary filePath template) = do
-        fmap (uncurry (FileHandle ∘ Just)) $
-             (if isBinary then E.openBinaryTempFile else E.openTempFile)
-             filePath template
-#endif
-    -- TODO: I need to review the handling of standard files:
-    openResource (Std std) = return $ FileHandle Nothing $ stdHndl std
-
-    closeResource (FileHandle _ h) = E.hClose h
-
-
--- ** Opening files in a region
-
--- | A handy type synonym for a regional handle to an opened file parameterized
--- by the IOMode in which you opened the file and the region in which it was
--- created.
-type RegionalFileHandle ioMode r = RegionalHandle (File ioMode) r
 
 {-| Convenience function for opening a file which yields a regional handle to
 it. This provides a safer replacement for @System.IO.@'SIO.openFile'.
@@ -539,28 +457,6 @@ cast :: forall anyIOMode castedIOMode
 
 However I'm not sure yet how to implement it...
 -}
-
---------------------------------------------------------------------------------
--- Utility wrapping functions
---------------------------------------------------------------------------------
-
-regularHandle ∷ RegionalFileHandle ioMode r → E.Handle ioMode
-regularHandle (internalHandle → FileHandle _ h) = h
-
-wrap ∷ MonadIO m
-     ⇒ (E.Handle ioMode → IO α)
-     → (RegionalFileHandle ioMode r → m α)
-wrap f = \h → liftIO $ f (regularHandle h)
-
-wrap2 ∷ MonadIO m
-      ⇒ (E.Handle ioMode → β → IO α)
-      → (RegionalFileHandle ioMode r → β → m α)
-wrap2 f = \h y → liftIO $ f (regularHandle h) y
-
-wrap3 ∷ MonadIO m
-      ⇒ (E.Handle ioMode → γ → β → IO α)
-      → (RegionalFileHandle ioMode r → γ → β → m α)
-wrap3 f = \h z y → liftIO $ f (regularHandle h) z y
 
 
 -------------------------------------------------------------------------------
